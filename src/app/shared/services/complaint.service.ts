6a781f96e0c08741442c9dd7fc90b8f2ea612667
unknown <dakota148@gmail.com>
2024-06-21 12:33:59 -0700

    ng-crud: implementing complaint service scrub

--- src/app/shared/services/complaint.service.ts
+++ src/app/shared/services/complaint.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable, of } from "rxjs";
-import { ComplaintRequest, ComplaintResponse } from "src/app/models";
+import { Complaint, ComplaintRequest, ComplaintResponse } from "src/app/models";
+import { complaintTypes } from "src/app/models/status";
 
 @Injectable({ providedIn: 'root' })
 export class ComplaintService {
@@ -12,7 +13,21 @@ export class ComplaintService {
     }
 
     scrubComplaints(response: ComplaintResponse): ComplaintResponse {
-        return response;
+        var complaints = response.complaints.map(x => this.scrub(x));
+        const result = <ComplaintResponse>{
+            complaints: complaints,
+            paging: response.paging
+        }
+        return result;
+    }
+
+    scrub(complaint: Complaint): any {
+        
+        if (complaint.events) {
+            complaint.events = complaint.events.sort((a, b) => new Date(b.timestamp).valueOf() - new Date(a.timestamp).valueOf());
+        }
+
+        return complaint;
     }
 
     getComplaints(request: ComplaintRequest): Observable<ComplaintResponse> {
