1a7e98cc2c75881cfc688456f16f768c2ab4beee
unknown <dakota148@gmail.com>
2024-06-24 14:06:34 -0700

    ng-crud: fixing material registration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, isDevMode } from '@angular/core';
+import { NgModule, isDevMode, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,16 +6,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+/** --- MATERIAL --- */
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
-// My Components
+//* --- My Components --- */
 
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ComplaintsComponent } from './pages/complaints/complaints.component';
 import { ComplaintsTableComponent } from './pages/complaints/complaints-table/complaints-table.component';
+// import { PagingComponent } from './components/paging/paging.component';
+
 
 // STORE
 import { StoreModule, provideStore } from '@ngrx/store';
@@ -24,25 +29,36 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule, provideEffects } from '@ngrx/effects';
 import { ComplaintsEffects } from './store/complaints/complaints.effects';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PagingComponent } from 'src/app/components/paging/paging.component';
 
 @NgModule({
   declarations: [
+    // layout
     AppComponent,
     HeaderComponent,
+
+    // pages
     ComplaintsComponent,
     HomeComponent,
     AboutComponent,
-    ComplaintsTableComponent
+    ComplaintsTableComponent,
+
+    // components
+    PagingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
 
     // Material-Components
     MatButtonModule,
     MatInputModule,
     MatCardModule,
+    MatPaginatorModule,
+    MatSlideToggleModule,
 
     // STORE
     StoreModule.forRoot({}, {}),
@@ -52,11 +68,15 @@ import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
     EffectsModule.forFeature([ComplaintsEffects]),
     // isDevMode() ? StoreDevtoolsModule.instrument() : [],
   ],
+  exports: [
+    MatPaginatorModule
+  ],
   providers: [
     provideHttpClient(withInterceptorsFromDi()),
     provideStore(),
     provideEffects([ComplaintsEffects])
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
